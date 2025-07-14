1fc08e2df3917ee3b3e46330f26be1be40faa968
archsinit <89917210+archsinit@users.noreply.github.com>
2025-07-14 21:05:17 +0900

    Create ubuntu-startup.sh

---
+++ ubuntu-startup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+# Ubuntu Server Hardening Interactive Script
+# Version: 0.1.0 | 2025-07-14
+# Changelog:
+#
+# Description:
+# This script is based on https://github.com/buildplan/du_setup/.
+# It is intended to be used on a fresh Ubuntu installation.
